173364ea2cc685b62d4ed64f13819ef9b586fa22
arianna coseschi <ariannacoseschi@gmail.com>
2023-11-23 01:16:57 +0000

    Second Attempt

--- higher_order_functions/higher_order_calculator.js
+++ higher_order_functions/higher_order_calculator.js
@@ -6,17 +6,18 @@ const sum = (number1, number2) ==> {
 const subtract = (number1,number2) ==> {
     console.log(number1 - number2);
 }
-
-//const doCalculation = (number1, number2, operation){
-    //if (operation === "sum"){
-     //   sum(number1, number2);
-   // }else if (operation === "subtract"){
-     //   subtract(number1,number2);
-   // }
+ 
+const doCalculation = (number1, number2, operation) ==>{
+    if (operation === "sum"){
+        sum(number1, number2);
+   }else if (operation === "subtract"){
+       subtract(number1,number2);
+   }
 
    const doCalculation = (number1, number2, callback) ==> {
     callback(number1, number2);
    }
+  }
 
     //doCalculation(2, 3, "sum");
 
